e110b2ca43835f6041ba148899bd9c5d72a38d4f
Musilda-Leopoldovna <Musilda-Leopoldovna@users.noreply.github.com>
2024-09-11 15:21:53 +0200

    Few additions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,11 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         Cat beanCat = (Cat) applicationContext.getBean("myau");
+        Cat beanCat2 = (Cat) applicationContext.getBean("myau");
 
         System.out.println(bean.getMessage());
         System.out.println(bean.getMessage());
+        System.out.printf("Links is equal: %s.", bean == bean2);
+        System.out.println();
         System.out.println(beanCat.getMessage());
         System.out.println(beanCat.getMessage());
+        System.out.printf("Links is equal: %s.", beanCat == beanCat2);
     }
 }
